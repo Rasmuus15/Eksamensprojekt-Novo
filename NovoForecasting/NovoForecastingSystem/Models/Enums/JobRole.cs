eab0bf2227ac0fb8d9956bd91b793106e218e871
Rexiedexy <jlundgren07@gmail.com>
2026-04-14 11:46:32 +0200

    Created classes

---
+++ NovoForecasting/NovoForecastingSystem/Models/Enums/JobRole.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NovoForecastingSystem.Models.Enums
+{
+    public enum JobRole
+    {
+        ProjectManager,
+        ProcessEngineer,
+        MechanicalEngineer,
+        ChemicalEngineer,
+        SoftwareEngineer,
+        MachineEngineer,
+        QualityAssurance,
+        MaintenanceEngineer,
+        OperationsManager,
+        FinancialAnalyst,
+        Designer,
+        Consultant
+
+    }
+}
---
+++ NovoForecasting/NovoForecastingSystem/Models/Enums/Phase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NovoForecastingSystem.Models.Enums
+{
+    public enum Phase
+    {
+        ConceptDesign,
+        BasicDesign,
+        DetailedDesign,
+        Manufacturing,
+        Installation,
+        Testing,
+        Approval,
+        GoLive
+    }
+}
