8f2bb392ce2acceb79493bd50b883121a63134b1
Rexiedexy <jlundgren07@gmail.com>
2026-04-16 12:25:00 +0200

    Added methods/variables

---
+++ NovoForecasting/NovoForecastingSystem/Repos/RessourceRepo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NovoForecastingSystem.Repos
+{
+    public class RessourceRepo
+    {
+        private List<Models.Ressource> ressource;
+
+        public RessourceRepo()
+        {
+            ressource = new List<Models.Ressource>(); 
+        }
+
+
+         public List<Models.Ressource> GetAllRessources()
+        {
+            return ressource;
+        }
+    }
+}
