c0ead1ec2fb65e5342974345097fc2d6bb00b272
spykiller103 <nbje74801@edu.ucl.dk>
2026-04-22 12:09:53 +0200

    Lavet et dataGrid

---
+++ NovoForecasting/NovoForecastingSystem/Views/Templates/FTEDemandByPhase&Role.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NovoForecastingSystem.Views.Templates
+{
+    /// <summary>
+    /// Interaction logic for FTEDemandByPhase_Role.xaml
+    /// </summary>
+    public partial class FTEDemandByPhase_Role : UserControl
+    {
+        public FTEDemandByPhase_Role()
+        {
+            InitializeComponent();
+        }
+    }
+}
