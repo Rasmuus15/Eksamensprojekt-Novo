20f41af60780b7ad87dfb98483ad89b538755b45
spykiller103 <nbje74801@edu.ucl.dk>
2026-04-27 09:14:24 +0200

    Fjernet sql commands

---
+++ NovoForecasting/NovoForecastingSystem/Views/CreateProjectWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Data.SqlClient;
+
+
+
+namespace NovoForecastingSystem.Views
+{
+    /// <summary>
+    /// Interaction logic for CreateProjectWindow.xaml
+    /// </summary>
+    public partial class CreateProjectWindow : Window
+    {
+
+        public CreateProjectWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
