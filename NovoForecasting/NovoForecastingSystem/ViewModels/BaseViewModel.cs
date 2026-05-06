ca9f1c9dc11526eda89ffafa19860f7b99c7307a
Spykiller <nicklasbjbj@gmail.com>
2026-05-06 20:26:09 +0200

    Removed never used code

--- NovoForecasting/NovoForecastingSystem/ViewModels/BaseViewModel.cs
+++ NovoForecasting/NovoForecastingSystem/ViewModels/BaseViewModel.cs
@@ -8,8 +8,6 @@ namespace NovoForecastingSystem.ViewModels
 {
     public abstract class BaseViewModel : INotifyPropertyChanged
     {
-        public virtual string Title { get; } = "";
-
         public event PropertyChangedEventHandler? PropertyChanged;
 
         protected void OnPropertyChanged([CallerMemberName] string name = null)
