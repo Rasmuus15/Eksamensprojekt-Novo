e5b1b4003a7be1aa697207a3104cceb2d6ad6f24
Spykiller <nicklasbjbj@gmail.com>
2026-05-13 10:10:43 +0200

    Removed unused code

--- NovoForecasting/NovoForecastingSystem/Models/Resource.cs
+++ NovoForecasting/NovoForecastingSystem/Models/Resource.cs
@@ -10,7 +10,6 @@ namespace NovoForecastingSystem.Models
         public int Id { get; set; }
         public string Initials { get; set; }
         public string Email { get; set; }
-        public double FTE { get; set; }
         public bool Availability { get; set; }
         public JobRole JobRoleEnum { get; set; }
         public int? ProjectId { get; set; }
